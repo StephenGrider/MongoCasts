aa21edd4c43c2e8b934c9d712ec162a8a53f874b
stephen grider <ste.grider@gmail.com>
2016-11-07 10:39:48 -0800

    added promise implementation

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/users_test');
-mongoose.connection
-  .once('open', () => console.log('Good to go!'))
-  .on('error', (error) => {
-    console.warn('Warning', error);
-  });
+mongoose.Promise = global.Promise;
+
+before((done) => {
+  mongoose.connect('mongodb://localhost/users_test');
+  mongoose.connection
+    .once('open', () => { done(); })
+    .on('error', (error) => {
+      console.warn('Warning', error);
+    });
+});
 
 beforeEach((done) => {
   mongoose.connection.collections.users.drop(() => {
