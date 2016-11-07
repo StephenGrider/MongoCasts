3d06645d95c002e7fa2373e23196964240e007d9
stephen grider <ste.grider@gmail.com>
2016-11-07 11:14:53 -0800

    added tostring

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -13,7 +13,7 @@ describe('Reading users out of the database', () => {
   it('finds all users with a name of joe', (done) => {
     User.find({ name: 'Joe' })
       .then((users) => {
-        console.log(users);
+        assert(users[0]._id.toString() === joe._id.toString());
         done();
       });
   });
