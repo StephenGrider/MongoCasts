6f1761d90f5af6aa7f5779fcbcf6dba69fb9d9a1
stephen grider <ste.grider@gmail.com>
2016-11-07 11:51:42 -0800

    findone

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -17,4 +17,12 @@ describe('Reading users out of the database', () => {
         done();
       });
   });
+
+  it('find a user with a particular id', (done) => {
+    User.findOne({ _id: joe._id })
+      .then((user) => {
+        assert(user.name === 'Joe');
+        done();
+      });
+  });
 });
