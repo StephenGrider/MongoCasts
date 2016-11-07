552c5aec4f2f82735b9670de6593d8e701429b40
stephen grider <ste.grider@gmail.com>
2016-11-07 15:14:19 -0800

    update all records via class

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -28,4 +28,25 @@ describe('Updating records', () => {
   it('A model instance can update', (done) => {
     assertName(joe.update({ name: 'Alex' }), done);
   });
+
+  it('A model class can update', (done) => {
+    assertName(
+      User.update({ name: 'Joe' }, { name: 'Alex' }),
+      done
+    );
+  });
+
+  it('A model class can update one record', (done) => {
+    assertName(
+      User.findOneAndUpdate({ name: 'Joe' }, { name: 'Alex' }),
+      done
+    );
+  });
+
+  it('A model class can find a record with an Id and update', (done) => {
+    assertName(
+      User.findByIdAndUpdate(joe._id, { name: 'Alex' }),
+      done
+    );
+  })
 });
