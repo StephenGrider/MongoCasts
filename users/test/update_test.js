5fb29230cd766491d5ae703f65bc92963f78a6a7
stephen grider <ste.grider@gmail.com>
2016-11-07 14:48:29 -0800

    added set and save test

---
+++ users/test/update_test.js
@@ -0,0 +1,23 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Updating records', () => {
+  let joe;
+
+  beforeEach((done) => {
+    joe = new User({ name: 'Joe' });
+    joe.save()
+      .then(() => done());
+  });
+
+  it('instance type using set n save', (done) => {
+    joe.set('name', 'Alex');
+    joe.save()
+      .then(() => User.find({}))
+      .then((users) => {
+        assert(users.length === 1);
+        assert(users[0].name === 'Alex');
+        done();
+      });
+  });
+});
