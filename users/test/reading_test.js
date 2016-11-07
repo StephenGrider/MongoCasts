9944ad37f708f522124f0e08a5387b53ff1b73c8
stephen grider <ste.grider@gmail.com>
2016-11-07 10:53:17 -0800

    added read test

---
+++ users/test/reading_test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Reading users out of the database', () => {
+  let joe;
+
+  beforeEach((done) => {
+    joe = new User({ name: 'Joe' });
+    joe.save()
+      .then(() => done());
+  });
+
+  it('finds all users with a name of joe', () => {
+    
+  });
+});
