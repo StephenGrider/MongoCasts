d4813c9882708ec4124ed58ee1d8a70809e76126
stephen grider <ste.grider@gmail.com>
2016-11-08 11:11:51 -0800

    removed subdoc

--- users/test/subdocument_test.js
+++ users/test/subdocument_test.js
@@ -34,4 +34,24 @@ describe('Subdocuments', () => {
         done();
       });
   });
+
+  it('can remove an existing subdocument', (done) => {
+    const joe = new User({
+      name: 'Joe',
+      posts: [{ title: 'New Title' }]
+    });
+
+    joe.save()
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        const post = user.posts[0];
+        post.remove();
+        return user.save();
+      })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.posts.length === 0);
+        done();
+      });
+  });
 });
