053727d897a13f925d976b21003b11a0bb4a7422
stephen grider <ste.grider@gmail.com>
2016-11-08 15:35:43 -0800

    added has one and has many relationships

--- users/test/association_test.js
+++ users/test/association_test.js
@@ -1,8 +1,19 @@
 const mongoose = require('mongoose');
-const User = require('./src/user');
-const Comment = require('./src/comment');
-const BlogPost = require('./src/blogPost');
+const User = require('../src/user');
+const Comment = require('../src/comment');
+const BlogPost = require('../src/blogPost');
 
 describe('Assocations', () => {
-  
+  let joe, blogPost, comment;
+
+  beforeEach((done) => {
+    joe = new User({ name: 'Joe' });
+    blogPost = new BlogPost({ title: 'JS is Great', content: 'Yep it really is' });
+    comment = new Comment({ content: 'Congrats on great post' });
+
+    joe.blogPosts.push(blogPost);
+    blogPost.comments.push(comment);
+    comment.user = joe;
+
+  });
 });
--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -12,10 +12,10 @@ before((done) => {
 });
 
 beforeEach((done) => {
-  const { users, comments, blogPosts } = mongoose.connection.collections;
+  const { users, comments, blogposts } = mongoose.connection.collections;
   users.drop(() => {
     comments.drop(() => {
-      blogPosts.drop(() => {
+      blogposts.drop(() => {
         done();
       });
     });
