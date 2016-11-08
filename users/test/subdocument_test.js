424f3da2dcf2280b11e5c73b02a3d0f76d456ec2
stephen grider <ste.grider@gmail.com>
2016-11-08 11:01:45 -0800

    added subdoc

--- users/test/subdocument_test.js
+++ users/test/subdocument_test.js
@@ -15,4 +15,23 @@ describe('Subdocuments', () => {
         done();
       });
   });
+
+  it('Can add subdocuments to an existing record', (done) => {
+    const joe = new User({
+      name: 'Joe',
+      posts: []
+    });
+
+    joe.save()
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        user.posts.push({ title: 'New Post' });
+        return user.save();
+      })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.posts[0].title === 'New Post');
+        done();
+      });
+  });
 });
