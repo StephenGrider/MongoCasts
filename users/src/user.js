895ea00b1b1330ca39d53fc3f7afbea8ed01a206
stephen grider <ste.grider@gmail.com>
2016-11-07 16:23:28 -0800

    added test

--- users/src/user.js
+++ users/src/user.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  name: String
+  name: String,
+  postCount: Number
 });
 
 const User = mongoose.model('user', UserSchema);
--- users/test/update_test.js
+++ users/test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
   let joe;
 
   beforeEach((done) => {
-    joe = new User({ name: 'Joe' });
+    joe = new User({ name: 'Joe', postCount: 0 });
     joe.save()
       .then(() => done());
   });
@@ -48,5 +48,9 @@ describe('Updating records', () => {
       User.findByIdAndUpdate(joe._id, { name: 'Alex' }),
       done
     );
-  })
+  });
+
+  it('A user can have their postcount incremented by 1', () => {
+    User.update({ name: 'Joe' }, { postCount: 1 });
+  });
 });
