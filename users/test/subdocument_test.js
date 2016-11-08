32691a05cad1d4af1f6f81c12337b4beb5ba6db2
stephen grider <ste.grider@gmail.com>
2016-11-08 10:44:35 -0800

    added subdocuments

---
+++ users/test/subdocument_test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Subdocuments', () => {
+  it('can create a subdocument', (done) => {
+    const joe = new User({
+      name: 'Joe',
+      posts: [{ title: 'PostTitle' }]
+    });
+
+    joe.save()
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.posts[0].title === 'PostTitle');
+        done();
+      });
+  });
+});
