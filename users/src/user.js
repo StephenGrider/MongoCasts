a0995108c5684eb3452650c6b7be907a3168939a
stephen grider <ste.grider@gmail.com>
2016-11-07 17:02:43 -0800

    require name

--- users/src/user.js
+++ users/src/user.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  name: String,
+  name: {
+    type: String,
+    required: [true, 'Name is required.']
+  },
   postCount: Number
 });
 
--- users/test/validation_test.js
+++ users/test/validation_test.js
@@ -3,6 +3,10 @@ const User = require('../src/user');
 
 describe('Validating records', () => {
   it('requires a user name', () => {
-    
+    const user = new User({ name: undefined });
+    const validationResult = user.validateSync();
+    const { message } = validationResult.errors.name;
+
+    assert(message === 'Name is required.');
   });
 });
