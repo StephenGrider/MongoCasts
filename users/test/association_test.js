5bcf836fc8471accb7f028da704c619fcfbf1bda
stephen grider <ste.grider@gmail.com>
2016-11-08 16:48:55 -0800

    added populate

--- users/test/association_test.js
+++ users/test/association_test.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const assert = require('assert');
 const User = require('../src/user');
 const Comment = require('../src/comment');
 const BlogPost = require('../src/blogPost');
@@ -19,10 +20,11 @@ describe('Assocations', () => {
       .then(() => done());
   });
 
-  it.only('saves a relation between a user and a blogpost', (done) => {
-    User.findOne({ name: 'Joe' }).then()
+  it('saves a relation between a user and a blogpost', (done) => {
+    User.findOne({ name: 'Joe' })
+      .populate('blogPosts')
       .then((user) => {
-        console.log(user);
+        assert(user.blogPosts[0].title === 'JS is Great');
         done();
       });
   });
