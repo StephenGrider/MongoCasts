d48570d6f3689bb846622c67b8bd17f510e9a7e9
stephen grider <ste.grider@gmail.com>
2016-11-09 15:45:51 -0800

    added artist model

--- UpStarMusic/database/models/artist.js
+++ UpStarMusic/database/models/artist.js
@@ -1,1 +1,20 @@
-// Todo: Create Artist Model
+const mongoose = require('mongoose');
+const AlbumSchema = require('./album');
+const Schema = mongoose.Schema;
+
+const ArtistSchema = new Schema({
+  name: String,
+  age: Number,
+  yearsActive: Number,
+  image: String,
+  genre: String,
+  website: String,
+  netWorth: Number,
+  labelName: String,
+  retired: Boolean,
+  albums: [AlbumSchema]
+});
+
+const Artist = mongoose.model('artist', ArtistSchema);
+
+module.exports = Artist;
