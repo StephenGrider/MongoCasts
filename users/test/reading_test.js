ceb788ee46f448a19f011c509b229b29f3c98c8f
stephen grider <ste.grider@gmail.com>
2016-11-09 13:58:21 -0800

    added sort test

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -2,11 +2,15 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Reading users out of the database', () => {
-  let joe;
+  let joe, maria, alex, zach;
 
   beforeEach((done) => {
+    alex = new User({ name: 'Alex' });
     joe = new User({ name: 'Joe' });
-    joe.save()
+    maria = new User({ name: 'Maria' });
+    zach = new User({ name: 'Zach' });
+
+    Promise.all([joe.save(), alex.save(), maria.save(), zach.save()])
       .then(() => done());
   });
 
@@ -22,6 +26,19 @@ describe('Reading users out of the database', () => {
     User.findOne({ _id: joe._id })
       .then((user) => {
         assert(user.name === 'Joe');
+        done();
+      });
+  });
+
+  it('can skip and limit the result set', (done) => {
+    User.find({})
+      .sort({ name: 1 })
+      .skip(1)
+      .limit(2)
+      .then((users) => {
+        assert(users.length === 2);
+        assert(users[0].name === 'Joe');
+        assert(users[1].name === 'Maria');
         done();
       });
   });
