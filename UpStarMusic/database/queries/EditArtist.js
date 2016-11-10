f0e921c0b2d0638411f88b44c45c23a017510ea0
stephen grider <ste.grider@gmail.com>
2016-11-10 15:29:53 -0800

    edit single artist

--- UpStarMusic/database/queries/EditArtist.js
+++ UpStarMusic/database/queries/EditArtist.js
@@ -7,4 +7,5 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves when the record is edited
  */
 module.exports = (_id, artistProps) => {
+  return Artist.update({ _id }, artistProps);
 };
