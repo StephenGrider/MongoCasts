6d8abbe10ed0ae3137bb585a4ba837eb6764492c
stephen grider <ste.grider@gmail.com>
2016-11-11 14:33:26 -0800

    added retired queries

--- UpStarMusic/database/queries/SetNotRetired.js
+++ UpStarMusic/database/queries/SetNotRetired.js
@@ -6,4 +6,9 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves after the update
  */
 module.exports = (_ids) => {
+  return Artist.update(
+    { _id: { $in: _ids } },
+    { retired: false },
+    { multi: true }
+  );
 };
