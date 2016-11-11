438b7cc8abc87095252b219f8ecc4a217634ef03
stephen grider <ste.grider@gmail.com>
2016-11-11 14:58:22 -0800

    count filtered set

--- UpStarMusic/database/queries/SearchArtists.js
+++ UpStarMusic/database/queries/SearchArtists.js
@@ -15,7 +15,7 @@ module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
     .skip(offset)
     .limit(limit);
 
-  return Promise.all([query, Artist.count()])
+  return Promise.all([query, Artist.find(buildQuery(criteria)).count()])
     .then((results) => {
       return {
         all: results[0],
