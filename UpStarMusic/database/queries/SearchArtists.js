8ff75082eb14788e43a17c4cda65de7da5d27fe9
stephen grider <ste.grider@gmail.com>
2016-11-11 11:56:38 -0800

    added base query

--- UpStarMusic/database/queries/SearchArtists.js
+++ UpStarMusic/database/queries/SearchArtists.js
@@ -7,6 +7,19 @@ const Artist = require('../models/artist');
  * @param {integer} offset How many records to skip in the result set
  * @param {integer} limit How many records to return in the result set
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
+ * like this: { all: [artists], count: count, offset: offset, limit: limit }
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+  // Write a query that will follow sort, offset, limit options only
+  // do not worry about 'criteria' yet
+
+  const query = Artist.find({})
+    .sort({ [sortProperty]: 1 })
+    .skip(offset)
+    .limit(limit);
+
+  return Promise.all([query, Artist.count()])
+    .then((results) => {
+      return { all: results[0], count: results[1], offset: offset, limit: limit };
+    });
 };
