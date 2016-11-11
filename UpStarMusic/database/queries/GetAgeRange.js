2e3b96f21969e4d021f95202679fd438b88ac0f1
stephen grider <ste.grider@gmail.com>
2016-11-11 11:12:21 -0800

    added min and max queries

--- UpStarMusic/database/queries/GetAgeRange.js
+++ UpStarMusic/database/queries/GetAgeRange.js
@@ -6,4 +6,20 @@ const Artist = require('../models/artist');
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
+  const minQuery = Artist
+    .find({})
+    .sort({ age: 1 })
+    .limit(1)
+    .then(artists => artists[0].age);
+
+  const maxQuery = Artist
+    .find({})
+    .sort({ age: -1 })
+    .limit(1)
+    .then(artists => artists[0].age);
+
+  return Promise.all([minQuery, maxQuery])
+    .then((result) => {
+      return { min: result[0], max: result[1] };
+    });
 };
--- UpStarMusic/database/queries/GetYearsActiveRange.js
+++ UpStarMusic/database/queries/GetYearsActiveRange.js
@@ -6,4 +6,20 @@ const Artist = require('../models/artist');
  * containing the min and max yearsActive, like { min: 0, max: 14 }.
  */
 module.exports = () => {
+  const minQuery = Artist
+    .find({})
+    .sort({ yearsActive: 1 })
+    .limit(1)
+    .then(artists => artists[0].yearsActive);
+
+  const maxQuery = Artist
+    .find({})
+    .sort({ yearsActive: -1 })
+    .limit(1)
+    .then(artists => artists[0].yearsActive);
+
+  return Promise.all([minQuery, maxQuery])
+    .then((result) => {
+      return { min: result[0], max: result[1] };
+    });
 };
