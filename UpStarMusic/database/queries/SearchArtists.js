63467c3b30a2b998d142792e4fe4255f7206cbed
stephen grider <ste.grider@gmail.com>
2016-11-11 13:22:50 -0800

    added text search

--- UpStarMusic/database/queries/SearchArtists.js
+++ UpStarMusic/database/queries/SearchArtists.js
@@ -29,6 +29,10 @@ module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
 const buildQuery = (criteria) => {
   const query = {};
 
+  if (criteria.name) {
+    query.$text = { $search: criteria.name };
+  }
+
   if (criteria.age) {
     query.age = {
       $gte: criteria.age.min,
