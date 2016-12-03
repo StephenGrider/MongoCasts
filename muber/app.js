29812207c1dfcd8264b172b353872e52d4fa8c9f
stephengrider <ste.grider@gmail.com>
2016-12-02 17:11:46 -0800

    added route handler

--- muber/app.js
+++ muber/app.js
@@ -3,4 +3,10 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+// Run this function whenever someone goes to
+// localhost:3050/
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
 module.exports = app;
