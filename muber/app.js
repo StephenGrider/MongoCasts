234f90bdd18b1f26ee3f990d9397f8f3e611c27d
stephengrider <ste.grider@gmail.com>
2016-12-05 11:30:42 -0800

    added bodyparser

--- muber/app.js
+++ muber/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const routes = require('./routes/routes');
 
 const app = express();
 
+app.use(bodyParser.json());
 routes(app);
 
 module.exports = app;
