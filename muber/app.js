c9306827d5e358661be869d80a11d3e51c894c85
stephengrider <ste.grider@gmail.com>
2016-12-06 17:45:13 -0800

    added test env

--- muber/app.js
+++ muber/app.js
@@ -6,7 +6,9 @@ const routes = require('./routes/routes');
 const app = express();
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/muber');
+if (process.env.NODE_ENV !== 'test') {
+  mongoose.connect('mongodb://localhost/muber');
+}
 
 app.use(bodyParser.json());
 routes(app);
--- muber/test/test_helper.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-beforeEach((done) => {
-  const { drivers } = mongoose.connection.collections;
-  drivers.drop()
-    .then(() => done());
-});
