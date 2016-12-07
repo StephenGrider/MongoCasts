6880514f56dd2e8669807f66170dee41c6623832
stephengrider <ste.grider@gmail.com>
2016-12-07 13:58:23 -0800

    added geojson schema

--- muber/models/Driver.js
+++ muber/models/Driver.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const PointSchema = new Schema({
+  coordinates: { type: [Number], index: '2dsphere' },
+  type: { type: String, default: 'Point' }
+});
+
 const DriverSchema = new Schema({
   email: {
     type: String,
@@ -9,7 +14,8 @@ const DriverSchema = new Schema({
   driving: {
     type: Boolean,
     default: false
-  }
+  },
+  geometry: PointSchema
 });
 
 const Driver = mongoose.model('driver', DriverSchema);
