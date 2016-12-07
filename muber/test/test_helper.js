f326894ff844c413fd12547c3930f9c5d9021055
stephengrider <ste.grider@gmail.com>
2016-12-06 20:19:06 -0800

    added test db

---
+++ muber/test/test_helper.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+before(done => {
+  mongoose.connect('mongodb://localhost/muber_test');
+  mongoose.connection
+    .once('open', () => done())
+    .on('error', error => {
+      console.warn('Warning', error);
+    });
+});
+
+beforeEach(done => {
+  const { drivers } = mongoose.connection.collections;
+  drivers.drop()
+    .then(() => done())
+    .catch(() => done());
+});
